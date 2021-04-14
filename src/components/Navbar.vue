b6e6ae2b59bd6020eaa112b2995edf253d6a07d3
halik023 <skyhalik@gmail.com>
2021-04-14 16:02:21 +0700

    navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,38 @@
+<template>
+<div>
+    <nav class="navbar navbar-expand-sm nav-bb">
+        <div class="collapse navbar-collapse justify-content-center" id="collapsibleNavbar">
+            <ul class="navbar-nav d-flex">
+                <li class="nav-item ml-4 mr-4">
+                    {{infoNavbar.pages}} Pages
+                </li>
+                <li class="nav-item ml-4 mr-4 li-main">
+                    {{infoNavbar.sheets}} Sheets
+                </li>
+                <li class="nav-item ml-4 mr-4">
+                    {{infoNavbar.screens}} Screens
+                </li>
+            </ul>
+        </div>
+    </nav>
+</div>
+</template>
+
+<script>
+export default {
+    props: {
+        infoNavbar: Object
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+.li-main {
+    font-weight: 600;
+    color: cadetblue;
+}
+
+.nav-bb {
+    border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+}
+</style>
--- src/views/Manager.vue
+++ src/views/Manager.vue
@@ -1,16 +1,42 @@
 <template>
 <div>
     <Header />
+    <Navbar :infoNavbar="infoNavbar"/>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-10">
+            </div>
+            <div class="col-2">
+            </div>
+        </div>
+    </div>
 </div>
 </template>
 
 <script>
-// @ is an alias to /src
 import Header from '@/components/Header.vue'
+import Navbar from '@/components/Navbar.vue'
+import {
+    mapGetters
+} from 'vuex'
 
 export default {
+    computed: {
+        ...mapGetters(['getInfoProject']),
+        infoNavbar () {
+            return {
+                pages: this.getInfoProject.numberOfPages,
+                sheets: this.getInfoProject.numberOfPages / 2 + 2,
+                screens: this.getInfoProject.numberOfPages / 2 + 2,
+            }
+        }
+    },
     components: {
-        Header
+        Header,
+        Navbar
+    },
+    mounted() {
+
     }
 }
 </script>
